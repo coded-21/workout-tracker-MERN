c999248882e981c5d1203dced1ea2260f82c4609
coded-21 <coded.ha21@gmail.com>
2023-08-09 16:16:25 -0400

    user specific workout functionality added

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 
 // get all workouts
 const getWorkouts = async(req, res) => {
-    const workouts = await Workout.find({}).sort({createdAt: -1})
+    const user_id = req.user._id
+    const workouts = await Workout.find({ user_id }).sort({createdAt: -1})
     if(!workouts){
         return res.status(404).json({mssg: 'Workouts not found.'})
     }
@@ -30,7 +31,8 @@ const getWorkout = async (req, res) => {
 const postWorkout = async (req, res) => {
     const { title, load, reps } = req.body
     try {
-        const workout = await Workout.create({ title, load, reps})
+        const user_id = req.user._id
+        const workout = await Workout.create({ title, load, reps, user_id})
         res.status(200).json(workout)
     } catch(error) {
         res.status(400).json({error: error.message})
