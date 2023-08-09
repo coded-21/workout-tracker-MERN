609a421d72e0c38f8e7fa1d9cc7775a2bcecdf03
coded-21 <coded.ha21@gmail.com>
2023-08-08 22:45:52 -0400

    added jwt authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,17 +1,36 @@
-const mongoose = require('mongoose');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 // log-in user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+
+    const {email, password} = req.body;
+
+    try {
+        user = await User.login(email, password)
+
+        // create token
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 // register
 const registerUser = async (req, res) => {
-    const { email, password } = req.body;
 
+    const { email, password } = req.body;
     try {
         const user = await User.register(email, password)
-        res.status(200).json({email, user})
+
+        // create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
+const { findOne } = require('./workoutModel');
 
 const Schema = mongoose.Schema
 
@@ -44,6 +45,26 @@ userSchema.statics.register = async function (email, password) {
     })
 
     return user;
+}
+
+// static login method
+userSchema.statics.login = async function(email, password) {
+
+    if (!email || !password) {
+        throw Error('All fields must be filled.')
+    }
+
+    const user = await this.findOne({email});
+    if(!user) {
+        throw Error('Incorrect email.')
+    }
+    
+    const match = await bcrypt.compare(password, user.password)
+    if(!match) {
+        throw Error('Incorrect password.')
+    }
+    return user
+
 }
 
 module.exports = mongoose.model('User', userSchema)
